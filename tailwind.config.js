194b839a1656daace6e371e7cf6885e2b45d276d
Borris <borris@jungle.co>
2024-01-31 09:39:25 +0400

    add how it works with tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./src/**/*.{html,js}",
+    "./src/how-it-works.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
