c0a7db8a033a7d3939f20cbb24d7482d442ee6c9
Borris <borris@jungle.co>
2024-02-01 09:16:14 +0400

    update transition and design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // "./src/**/*.{html,js}",
+    "./src/**/*.{html,js}",
     "./src/how-it-works.html",
+    "./src/how-it-works copy.html",
   ],
   theme: {
     extend: {},
