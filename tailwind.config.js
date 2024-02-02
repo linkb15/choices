358ba500956d9e6875ab16debd2cc0493383b467
Borris <borris@jungle.co>
2024-02-02 10:27:35 +0400

    update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    "./src/how-it-works.html",
-    "./src/how-it-works copy.html",
+    './src/**/*.{html,js}',
+    './src/how-it-works.html',
+    './src/how-it-works copy.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          800: '#222',
+        },
+        sky: {
+          500: '#239BFF',
+        },
+        zinc: {
+          500: '#808080',
+        },
+        green: {
+          500: '#6FC07C',
+        },
+        slate: {
+          100: '#F5F6FA',
+        },
+        gray: {
+          100: '#F1F1F3',
+        },
+      },
+    },
   },
   plugins: [],
 }
